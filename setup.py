23f59c9c9b00910dd165a0c693a5be218753f5b5
ralbert8 <ralbert8@umd.edu>
2025-08-19 11:56:14 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,9 +14,9 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='ralbert',
-    maintainer_email='ralbert@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer_email='rileyalbert00@gmail.com',
+    description='Simulates open-loop control of a TurtleBot.',
+    license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
